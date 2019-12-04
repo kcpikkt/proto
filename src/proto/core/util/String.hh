ce45909ac48c51ec139fd66116258279fc57fb0b
Kacper <kacper.kokot.44@gmail.com>
2019-12-04 11:57:30 +0100

    memoverflow fixed, stop null-terminating StringViews?

---
+++ src/proto/core/util/String.hh
@@ -0,0 +1,28 @@
+#pragma once
+#include "proto/core/common.hh"
+#include "proto/core/util/StringView.hh"
+#include "proto/core/DataholderCRTP.hh"
+#include "proto/core/debug/assert.hh"
+#include "proto/core/containers/Array.hh"
+#include "proto/core/string.hh"
+
+namespace proto {
+    
+    // NOT NECESSARILY NULL TERMINATED!
+    struct String : Array<char> {
+        void init(StringView view, memory::Allocator * allocator) {
+            init_resize(view.length() + 1, allocator);
+            strview_copy(_data, view);
+        }
+        
+        StringView view() {
+            return StringView(_data, size()); 
+        }
+
+        operator bool() { return _data; }
+
+        operator StringView() {
+            return view();
+        }
+    };
+} // namespace proto
