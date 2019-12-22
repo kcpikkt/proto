a5fdacf33cf055519e41af9b9edc0877fb4b9551
kcpikkt <kacper.kokot.44@gmail.com>
2019-12-22 07:57:45 +0000

    Fixing merge bugs caused by MSWindows case insensitivity

--- src/proto/core/util/Bitfield.hh
+++ src/proto/core/util/Bitfield.hh
@@ -1,19 +1,32 @@
 #pragma once
+namespace proto {
+    template<typename T>
+    struct Bitfield {
+        T _field = 0;
+        void set   (T option) { _field |= option; }
+        void unset (T option) { _field &= ~option; }
+        void toggle(T option) { _field ^= option; }
+        bool check (T option) const { return (_field & option); }
 
-namespace proto{
-    // todo enable_if is integral unsigned
-    template<typename I>
-    static inline void bitfield_set(I * bitfield, I flag) {
-        *bitfield |= flag;
-    }
+        Bitfield() : _field(0) {}
 
-    template<typename I>
-    static inline void bitfield_unset(I * bitfield, I flag) {
-        *bitfield &= ~flag;
-    }
+        ~Bitfield() {}
 
-    template<typename I>
-    static inline void bitfield_toggle(I * bitfield, I flag) {
-        *bitfield ^= flag;
-    }
+        Bitfield(T value) {
+            _field = value;}
+
+        Bitfield<T>& operator=(T value) {
+            _field = value;
+            return *this;
+        }
+
+        Bitfield(const Bitfield<T>& other) {
+            _field = other._field;}
+
+        Bitfield<T> operator=(Bitfield<T>& other) {
+            _field = other._field;
+            return *this;
+        }
+    };
 } // namespace proto
+
